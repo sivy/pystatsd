91a42eb5cfa27ad4c4527d168a2030ffe05d9306
Steve Ivy <sivy@sixapart.com>
2011-02-16 09:17:46 -0700

    initial import

---
+++ pystatsd/statsd.py
@@ -0,0 +1,86 @@
+# statsd.py
+
+# Steve Ivy <steveivy@gmail.com>
+# http://monkinetic.com
+
+from socket import *
+
+# Sends statistics to the stats daemon over UDP
+class Statsd(object):
+    
+    def __init__(self, host='localhost', port=8125):
+        """
+        Create a new Statsd client.
+        * host: the host where statsd is listening, defaults to localhost
+        * port: the port where statsd is listening, defaults to 8125
+
+        >>> from pystatsd import statsd
+        >>> stats_client = statsd.Statsd(host, port)
+        """
+        self.host = host
+        self.port = port
+    
+    def timing(self, stats, time, sample_rate=1):
+        """
+        Log timing information
+        >>> from pystatsd import statsd
+        >>> statsd_client.timing('some.time','500|ms')
+        """
+        self.update_stats(self, stats, time, sample_rate)
+
+    def increment(self, stats, sample_rate=1):
+        """
+        Increments one or more stats counters
+        >>> statsd_client.increment('some.int')
+        >>> statsd_client.increment('some.int',0.5)
+        """
+        self.update_stats(self, stats, 1, sample_rate)
+
+    def decrement(self, stats, sample_rate=1):
+        """
+        Decrements one or more stats counters
+        >>> statsd_client.decrement('some.int')
+        """
+        self.update_stats(self, stats, -1, sample_rate)
+    
+    def update_stats(self, stats, delta=1, sampleRate=1):
+        """
+        Updates one or more stats counters by arbitrary amounts
+        >>> statsd_client.update_stats('some.int',10)
+        """
+        if (type(stats) is not list):
+            stats = [stats]
+        data = {}
+        for stat in stats:
+            data[stat] = "%s|c" % delta
+
+        self.send(data, sampleRate)
+    
+    def send(self, data, sample_rate=1):
+        """
+        Squirt the metrics over UDP
+        """
+        addr=(self.host, self.port)
+        
+        sampled_data = {}
+        
+        if(sample_rate < 1):
+            import random
+            if random.random() <= sample_rate:
+                for stat in data.keys():
+                    value = data[stat]
+                    sampled_data[stat] = "%s|@%s" %(value, sample_rate)
+        else:
+            sampled_data=data
+        
+        udp_sock = socket(AF_INET, SOCK_DGRAM)
+        try:
+            for stat in sampled_data.keys():
+                value = data[stat]
+                send_data = "%s:%s" % (stat, value)
+                udp_sock.sendto(send_data, addr)
+        except:
+            import sys
+            from pprint import pprint
+            print "Unexpected error:", pprint(sys.exc_info())
+            pass # we don't care
---
+++ setup.py
@@ -0,0 +1,19 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "pystatsd",
+    version = "0.0.1",
+    author = "Steve Ivy",
+    author_email = "steveivy@gmail.com",
+    description = (""),
+    license = "BSD",
+    packages=['py-statsd'],
+    long_description=read('README'),
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+    ],
+)
