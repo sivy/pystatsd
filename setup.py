ecc51d66fa6034a6958727ef59735485896e45c2
Steve Ivy <steve@wallrazer.com>
2011-08-08 23:02:49 -0700

    version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name = "pystatsd",
-    version = "0.1.5",
+    version = "0.1.6",
     author = "Steve Ivy",
     author_email = "steveivy@gmail.com",
     description = ("pystatsd is a client for Etsy's statsd server, a front end/proxy for the Graphite stats collection and graphing server."),
