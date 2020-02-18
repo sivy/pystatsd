da78db870479fd78929ae8fdaad7a7499ad0e3c8
Christian Clauss <cclauss@me.com>
2020-02-18 17:35:51 +0100

    file() was removed in Python 3 in favor of open()

--- pystatsd/daemon.py
+++ pystatsd/daemon.py
@@ -49,9 +49,9 @@ class Daemon(object):
         # Redirect file descriptors.
         sys.stdout.flush()
         sys.stderr.flush()
-        si = file(self.stdin, 'r')
-        so = file(self.stdout, 'a+')
-        se = file(self.stderr, 'a+', 0)
+        si = open(self.stdin, 'r')
+        so = open(self.stdout, 'a+')
+        se = open(self.stderr, 'a+', 0)
         os.dup2(si.fileno(), sys.stdin.fileno())
         os.dup2(so.fileno(), sys.stdout.fileno())
         os.dup2(se.fileno(), sys.stderr.fileno())
